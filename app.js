781a27abbd4ffe76b1593144887867017ca9aeb8
tarawinn <tarawinnadams@gmail.com>
2018-02-17 17:54:51 -0500

    Added the thankyou page, inventoy updating

--- app.js
+++ app.js
@@ -28,45 +28,93 @@ app.get('/', function (req, res) {
 
 //create shopping cart for user, check inventory, calculate totals, render checkout page
 var prices = [];
-var subtotal = 0;
+var shipping = 0;
+var cart = [];
 app.post('/checkout', function (req,res){
-    console.log('Form (from querystring): ' + req.query.form);
 
-    //console.log(req.body);
-    //console.log(inventory);
     req.session.cart = req.body;
-    var cart = req.session.cart;
+    
+    for(i = 0; i < inventory.length; i++){
+        if(cart.length > 0 && cart[i] != null){
+            cart[i] = cart[i] + Number(req.session.cart["item"+i]);
+        }else cart[i] = Number(req.session.cart["item"+i]);
+    }
+    
 
+//calulate totals
+var total_pieces = 0;
+var subtotal = 0;
    for(i = 0; i < inventory.length; i++){
-        //console.log("$",inventory[i].price * cart["item"+i]);
-        prices[i] = inventory[i].price * cart["item"+i];
+        prices[i] = inventory[i].price * Number(cart[i]);
+        total_pieces = total_pieces + Number(cart[i]);
         subtotal = subtotal + prices[i];
         console.log("$",prices[i]);
     }
     console.log("$",subtotal);
+    console.log(total_pieces);
+    req.session.subtotal = subtotal;
+    req.session.total_pieces = total_pieces;
+    console.log(req.session.subtotal);
+    console.log(req.session.total_pieces);
+
+//calculate shipping
+    if(total_pieces < 5){
+        shipping = 5.00;
+    }else if(total_pieces > 5 && total_pieces < 11){
+        shipping = 7.50;
+    }else shipping = 10.00;
+    
+
     res.render('checkout',{
         inventory: inventory,
         cart: cart,
         prices: prices,
-        subtotal: subtotal
+        subtotal: subtotal,
+        shipping: shipping
     });
 
 });
 
-
-
 //edit shopping cart from checkout page
 app.post('/edit', function (req,res){
-
-
-    //res.sendFile(__dirname + '/views/index.html');
-    //res.send(req.session.cart);
-    
+    res.render('index',{
+        inventory: inventory
+    });
 });
 
 
 //submit purchase order, send user to thankyou page, update inventory
 
+app.post('/thankyou', function (req,res){
+    var subtotal = req.session.subtotal;
+    res.render('thankyou',{
+        inventory: inventory,
+        cart: cart,
+        prices: prices,
+        subtotal: subtotal,
+        shipping: shipping
+    });
+    
+//updating inventory
+
+    for(i = 0; i < inventory.length; i++){
+        inventory[i].quantityInStock = inventory[i].quantityInStock - Number(cart[i]);
+    }
+
+var jsonContent = JSON.stringify(inventory);
+
+fs.writeFile('./inventory.json', jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("Inventory has been updated.");
+});
+
+});
+
+//would like to figure out how to use this properly
 /*fs.readFile(require.resolve('./inventory.json'), "utf-8", (err, data) => {
     if (err) {
         throw err;
@@ -77,22 +125,7 @@ app.post('/edit', function (req,res){
 
 
 
-//updating inventory
-/*inventory[0].itemName = "cheese";
-console.log(inventory[0].itemName);
-console.log(inventory);
 
-var jsonContent = JSON.stringify(inventory);
-
-fs.writeFile('./inventory.json', jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-});
-*/
 
 //start server on port 3000
 app.listen(app.get('port'),function(){
