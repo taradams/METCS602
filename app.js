a81f4a0341c9de787effe1411921296104ba7d8b
tarawinn <tarawinnadams@gmail.com>
2018-02-17 16:02:06 -0500

    Added more functionality. Included EJS view engine. Also calculating cart totals

--- app.js
+++ app.js
@@ -5,50 +5,96 @@ var session = require('express-session');
 const fs = require("fs");
 
 
+
+//figure out how to organize these
 app.use(express.static(__dirname + '/views'));
 app.use(cookieParser());
 app.use(session({secret: "Shh, its a secret!"}));
 app.use(require('body-parser'). urlencoded({ extended: true }));
-app.set('port', process.env.PORT | 3000);
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
 
+//load inventory synchronously
+var inventory = JSON.parse(fs.readFileSync('./inventory.json'));
+
+//Render the home page
 app.get('/', function (req, res) {
-  res.sendFile('index.html')
+  res.render('index',{
+      inventory: inventory
+  });
+
 });
 
 
-
+//create shopping cart for user, check inventory, calculate totals, render checkout page
+var prices = [];
+var subtotal = 0;
 app.post('/checkout', function (req,res){
-    console.log(' Form (from querystring): ' + req.query.form);
-    console.log(' turtles: ' + req.body.turtles); 
-    console.log(' milk: ' + req.body.milk);
-    console.log(' pepper: ' + req.body.peppermint);
-    console.log(' espresso: ' + req.body.espresso);
-    console.log(' toffee: ' + req.body.toffee); 
+    console.log('Form (from querystring): ' + req.query.form);
 
-    req.session.cart = [req.body.turtles, req.body.milk,req.body.peppermint, req.body.espresso,req.body.toffee];
-    console.log(req.session.cart);
+    //console.log(req.body);
+    //console.log(inventory);
+    req.session.cart = req.body;
+    var cart = req.session.cart;
 
-    res.sendFile(__dirname + '/views/checkout.html');
+   for(i = 0; i < inventory.length; i++){
+        //console.log("$",inventory[i].price * cart["item"+i]);
+        prices[i] = inventory[i].price * cart["item"+i];
+        subtotal = subtotal + prices[i];
+        console.log("$",prices[i]);
+    }
+    console.log("$",subtotal);
+    res.render('checkout',{
+        inventory: inventory,
+        cart: cart,
+        prices: prices,
+        subtotal: subtotal
+    });
 
 });
 
 
+
+//edit shopping cart from checkout page
 app.post('/edit', function (req,res){
 
+
     //res.sendFile(__dirname + '/views/index.html');
-    res.send(req.session.cart);
+    //res.send(req.session.cart);
+    
 });
 
-fs.readFile(require.resolve('./inventory.json'), "utf-8", (err, data) => {
+
+//submit purchase order, send user to thankyou page, update inventory
+
+/*fs.readFile(require.resolve('./inventory.json'), "utf-8", (err, data) => {
     if (err) {
         throw err;
     }
     var inventory = JSON.parse(data);
-    console.log(inventory);
+}); */
+
+
+
+
+//updating inventory
+/*inventory[0].itemName = "cheese";
+console.log(inventory[0].itemName);
+console.log(inventory);
+
+var jsonContent = JSON.stringify(inventory);
+
+fs.writeFile('./inventory.json', jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("JSON file has been saved.");
 });
+*/
 
-
-
+//start server on port 3000
 app.listen(app.get('port'),function(){
     console.log(`Server is listening on port 3000`);
   });
