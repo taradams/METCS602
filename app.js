045c17a03077603856a9a17d2040c02d3d55d524
tarawinn <tarawinnadams@gmail.com>
2018-02-17 19:38:34 -0500

    Added functionality to edit order from checkout page

--- app.js
+++ app.js
@@ -32,15 +32,14 @@ var shipping = 0;
 var cart = [];
 app.post('/checkout', function (req,res){
 
-    req.session.cart = req.body;
-    
-    for(i = 0; i < inventory.length; i++){
-        if(cart.length > 0 && cart[i] != null){
-            cart[i] = cart[i] + Number(req.session.cart["item"+i]);
-        }else cart[i] = Number(req.session.cart["item"+i]);
-    }
-    
 
+    for(i = 0; i < inventory.length; i++){
+        if( cart.length > 0 && cart[i] != null){
+            cart[i] = cart[i] + Number(req.body["item"+i]);
+        }else cart[i] = Number(req.body["item"+i]);
+    }
+
+    console.log(cart);
 //calulate totals
 var total_pieces = 0;
 var subtotal = 0;
@@ -52,6 +51,7 @@ var subtotal = 0;
     }
     console.log("$",subtotal);
     console.log(total_pieces);
+    req.session.prices = prices;
     req.session.subtotal = subtotal;
     req.session.total_pieces = total_pieces;
     console.log(req.session.subtotal);
@@ -68,15 +68,56 @@ var subtotal = 0;
     res.render('checkout',{
         inventory: inventory,
         cart: cart,
-        prices: prices,
-        subtotal: subtotal,
+        prices: req.session.prices,
+        subtotal: req.session.subtotal,
         shipping: shipping
     });
 
 });
-
 //edit shopping cart from checkout page
 app.post('/edit', function (req,res){
+     
+    for(i = 0; i < inventory.length; i++){
+        if(req.body["item"+i] != null){
+            cart[i] = Number(req.body["item"+i]);
+        }
+    } 
+        console.log(cart);
+
+//calulate totals
+        var total_pieces = 0;
+        var subtotal = 0;
+    for(i = 0; i < inventory.length; i++){
+        prices[i] = inventory[i].price * Number(cart[i]);
+        total_pieces = total_pieces + Number(cart[i]);
+        subtotal = subtotal + prices[i];
+        console.log("$",prices[i]);
+    }
+    req.session.subtotal = subtotal;
+    req.session.total_pieces = total_pieces;
+//calculate shipping
+if(total_pieces < 5){
+    shipping = 5.00;
+}else if(total_pieces > 5 && total_pieces < 11){
+    shipping = 7.50;
+}else shipping = 10.00;
+    
+        res.render('checkout',{
+            inventory: inventory,
+            cart: cart,
+            prices: prices,
+            subtotal: subtotal,
+            shipping: shipping
+        });
+        
+
+    });
+
+
+
+
+//continue shopping
+app.post('/continue', function (req,res){
     res.render('index',{
         inventory: inventory
     });
